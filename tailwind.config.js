af5f01b39a02aab65abffbd634a799868bf7b3b8
Igor Beduin <beduinigor@gmail.com>
2022-11-23 15:53:38 -0300

    feat: adds custom colors to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        colors: {
+            primary: "#1A1B1F",
+            "primary-base-low": "#5F6063",
+            "primary-base-lowest": "#B8B8B8",
+            back: "#ECECEC",
+            "surface-high": "#D0D0D0",
+            neutral: "#F5F6F8",
+            "primary-neutral-low": "#7B7C7D",
+            "primary-neutral-high": "#323947",
+            "primary-neutral-lowest": "#B1B2B4",
+            surface: "#121212",
+            base: "#FFFFFF",
+            "surface-low": "#888888",
+        }
+    },
+    plugins: [],
+};
