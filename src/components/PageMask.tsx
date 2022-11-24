ed74aba6f5cbeecbb8bca3293bfb18d0c0d90639
Igor Beduin <beduinigor@gmail.com>
2022-11-24 09:32:01 -0300

    feat: organiza estrtura basica do header

--- src/components/PageMask.tsx
+++ src/components/PageMask.tsx
@@ -1,22 +1,32 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 export default function PageMask() {
     return (
-        <>
-            <header>
+        <div className="flex flex-col justify-start items-center bg-back min-h-screen h-fit">
+            <header className="flex justify-between bg-surface h-28 w-10/12 text-surface-low">
                 <section>
-                    <nav>
-                        {/* links para navegação de paginas */}
+                    <nav className="flex justify-evenly">
+                        <Link to="profile">Profile</Link>
+                        <Link to="learn">Learn</Link>
+                        <Link to="blog">Blog</Link>
+                        <Link to="bookmarks">Bookmarks</Link>
+                        <Link to="ui-kit">UI Kit</Link>
+                        <Link to="other">Lain Nya</Link>
                     </nav>
                 </section>
                 <section>
-                    {/* links para redes sociais */}
+                    <div>
+                        <Link to="">Twitter</Link>
+                        <Link to="">Instagram</Link>
+                        <Link to="">Dribbble</Link>
+                        <Link to="">Medium</Link>
+                    </div>
                 </section>
             </header>
-            <main>
+            <main className="w-10/12 h-fit">
                 <Outlet />
             </main>
-        </>
+        </div>
     );
 }
