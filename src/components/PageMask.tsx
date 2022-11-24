4dbda7156566c8c4b8f8142299ff68a211bba96c
Igor Beduin <beduinigor@gmail.com>
2022-11-24 15:10:35 -0300

    refact: utiliza componente custom de Header no PageMask

--- src/components/PageMask.tsx
+++ src/components/PageMask.tsx
@@ -1,29 +1,13 @@
 import React from "react";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+
+import Header from "./Header";
 
 export default function PageMask() {
+
     return (
         <div className="flex flex-col justify-start items-center bg-back min-h-screen h-fit font-poppins">
-            <header className="flex justify-between bg-surface h-28 w-10/12 text-surface-low">
-                <section>
-                    <nav className="flex justify-evenly">
-                        <Link to="profile">Profile</Link>
-                        <Link to="learn">Learn</Link>
-                        <Link to="blog">Blog</Link>
-                        <Link to="bookmarks">Bookmarks</Link>
-                        <Link to="ui-kit">UI Kit</Link>
-                        <Link to="other">Lain Nya</Link>
-                    </nav>
-                </section>
-                <section>
-                    <div>
-                        <Link to="">Twitter</Link>
-                        <Link to="">Instagram</Link>
-                        <Link to="">Dribbble</Link>
-                        <Link to="">Medium</Link>
-                    </div>
-                </section>
-            </header>
+            <Header />
             <main className="w-10/12 h-fit">
                 <Outlet />
             </main>
