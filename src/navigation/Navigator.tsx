27e7ab7fe1ca466513e725c50135806e0092a72d
Igor Beduin <beduinigor@gmail.com>
2022-11-24 15:12:58 -0300

    refact: renderiza componente BlankPage em componentes de rotas não implementadas

--- src/navigation/Navigator.tsx
+++ src/navigation/Navigator.tsx
@@ -1,24 +1,20 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import PageMask from "../components/PageMask";
+import BlankPage from "../pages/BlankPage";
 import Blog from "../pages/Blog";
-import Bookmarks from "../pages/Bookmarks";
-import Learn from "../pages/Learn";
-import Other from "../pages/Other";
-import Profile from "../pages/Profile";
-import UiKit from "../pages/UiKit";
 
 export default function Navigator() {
     return (
         <Routes>
             <Route path="/" element={<Navigate to="/blog" />} />
             <Route path="/" element={<PageMask />}>
-                <Route path="profile" element={<Profile />}/>
-                <Route path="learn" element={<Learn />}/>
+                <Route path="profile" element={<BlankPage />}/>
+                <Route path="learn" element={<BlankPage />}/>
                 <Route path="blog" element={<Blog />}/>
-                <Route path="bookmarks" element={<Bookmarks/>} />
-                <Route path="ui-kit" element={<UiKit />} />
-                <Route path="other" element={<Other />}/>
+                <Route path="bookmarks" element={<BlankPage/>} />
+                <Route path="ui-kit" element={<BlankPage />} />
+                <Route path="other" element={<BlankPage />}/>
             </Route>
         </Routes>
     );
