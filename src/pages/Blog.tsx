1bc0b3ace3835a2a060132c27b16147d4dc13112
Igor Beduin <beduinigor@gmail.com>
2022-11-25 08:40:09 -0300

    refact: usa map para render. de articles e adiciona design responsivo para telas pequenas

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,70 +1,84 @@
 import React from "react";
 
 import articles from "../consts/articles";
+import { ArticleInterface } from "../consts/articles";
 
 import blogLogo from "../assets/imgs/logo.png";
 import learnBanner from "../assets/imgs/learnbanner.png";
+import learnBannerSmall from "../assets/imgs/learnbanner_small.png";
 import BlogPost from "../components/BlogPost";
 
 export default function Blog() {
+    interface ArticlesCollectionInterface {
+        [category: string]: Array<ArticleInterface>
+    }
+
+    const articlesCollection: ArticlesCollectionInterface = {};
+    articles.forEach((article) => {
+        if (!Object.keys(articlesCollection).includes(article.category)) {
+            articlesCollection[article.category] = [article];
+        }
+        else {
+            articlesCollection[article.category].push(article);
+        }
+    });
 
     return (
         <section>
             <section className="bg-surface px-12 h-fit">
-                <figure className="flex justify-center items-center border border-solid border-y-border-dark border-x-0 h-fit">
+                <figure className="flex justify-center items-center border border-solid border-y-divider-dark border-x-0 h-fit">
                     <img className="my-12" src={blogLogo} alt="Blog Logo"/>
                 </figure>
             </section>
-            <section className="p-12 h-fit bg-surface">
-                <BlogPost article={articles[0]} variant='main-0' />
-                <BlogPost article={articles[1]} variant='main-1' />
-                <BlogPost article={articles[2]} variant='main-1' />
-                <BlogPost article={articles[3]} variant='main-1' />
+            <section className="grid p-12 h-fit bg-surface md:grid-cols-2 md:gap-x-12">
+                {articlesCollection.main.map((article, index) => {
+                    if (index === 0) {
+                        return <BlogPost key={`blogPost-main-${index}`} article={article} variant='main-0' />;
+                    }
+                    return <BlogPost divider={index > 1} key={`blogPost-main-${index}`} article={article} variant='main-1' />;
+                })}
             </section>
             <section className="p-12 h-fit bg-base">
                 <p className="text-lg font-bold text-primary-base-lowest mb-6">PILIHAN EDITOR</p>
-                <BlogPost article={articles[4]} variant='editor' />
-                <BlogPost article={articles[5]} variant='editor' />
-                <BlogPost article={articles[6]} variant='editor' />
-                <BlogPost article={articles[7]} variant='editor' />
+                <div className="flex flex-col justify-center md:justify-between items-center md:items-start md:flex-row">
+                    {articlesCollection["pilihan editor"].map((article, index) => (
+                        <BlogPost divider={!!index} key={`blogPost-editor-${index}`} article={article} variant='editor' />
+                    ))}
+                </div>
             </section>
             <section className="p-12 h-fit bg-neutral">
                 <p className="text-lg font-bold text-primary-neutral-lowest mb-6">ARTIKEL TERBARU</p>
-                <BlogPost article={articles[8]} variant='terbaru' />
-                <BlogPost article={articles[9]} variant='terbaru' />
-                <BlogPost article={articles[10]} variant='terbaru' />
-                <BlogPost article={articles[11]} variant='terbaru' />
-                <BlogPost article={articles[12]} variant='terbaru' />
-                <BlogPost article={articles[13]} variant='terbaru' />
+                <div className="flex flex-wrap">
+                    {articlesCollection["artikel terbaru"].map((article, index) => (
+                        <BlogPost divider={!!index} key={`blogPost-terbaru-${index}`} article={article} variant='terbaru' />
+                    ))}
+                </div>
             </section>
             <section className="h-fit">
                 <figure>
-                    <img src={learnBanner}>
-                    </img>
+                    <img className="hidden md:block" src={learnBanner} />
+                </figure>
+                <figure>
+                    <img className="block md:hidden" src={learnBannerSmall} />
                 </figure>
             </section>
-            <section className="p-12 h-fit bg-base">
-                <p className="text-lg font-bold text-primary-base-lowest mb-6">ARTIKEL LAINNYA</p>
-                <BlogPost article={articles[14]} variant='lainnya' />
-                <BlogPost article={articles[15]} variant='lainnya' />
-                <BlogPost article={articles[16]} variant='lainnya' />
-                <BlogPost article={articles[17]} variant='lainnya' />
-                <BlogPost article={articles[18]} variant='lainnya' />
-            </section>
-            <section className="p-12 h-fit bg-base">
-                <p className="text-lg font-bold text-primary-base-lowest mb-6">ARSIP 2021</p>
-                <BlogPost article={articles[19]} variant='arsip' />
-                <BlogPost article={articles[20]} variant='arsip' />
-                <BlogPost article={articles[21]} variant='arsip' />
-                <BlogPost article={articles[22]} variant='arsip' />
-                <BlogPost article={articles[23]} variant='arsip' />
-                <BlogPost article={articles[24]} variant='arsip' />
-                <BlogPost article={articles[25]} variant='arsip' />
-                <BlogPost article={articles[26]} variant='arsip' />
-                <BlogPost article={articles[27]} variant='arsip' />
-                <BlogPost article={articles[28]} variant='arsip' />
-                <BlogPost article={articles[29]} variant='arsip' />
-                <BlogPost article={articles[30]} variant='arsip' />
+            <section className="p-12 h-fit bg-base flex flex-col md:flex-row md:justify-between">
+                <section>
+                    <p className="text-lg font-bold text-primary-base-lowest mb-6">ARTIKEL LAINNYA</p>
+                    <div>
+                        {articlesCollection["artikel lainnya"].map((article, index) => (
+                            <BlogPost divider={!!index} key={`blogPost-lainnya-${index}`} article={article} variant='lainnya' />
+                        ))}
+                    </div>
+                </section>
+                <section className="md:ml-12">
+                    <p className="text-lg font-bold text-primary-base-lowest mb-6">ARSIP 2021</p>
+                    <div>
+                        {articlesCollection["arsip 2021"].map((article, index) => (
+                            <BlogPost divider={!!index} key={`blogPost-arsip-${index}`} article={article} variant='arsip' />
+                        ))}
+                    </div>
+                </section>
             </section>
         </section>
     );
